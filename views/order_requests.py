c43dc7fde7afb7009c85f8a0d981656314b9df00
Emily Nelson <evnelson2021@gmail.com>
2023-01-10 13:14:14 -0600

    initial push

---
+++ views/order_requests.py
@@ -0,0 +1,12 @@
+ORDERS = [
+    {
+        "id": 1,
+        "metal_id": 3,
+        "size_id": 2,
+        "style_id": 3,
+        "timestamp": 1614659931693
+    }
+]
+
+def get_all_orders():
+    return ORDERS
